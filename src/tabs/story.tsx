56465c100ee28477b3367b5fc9bf372eb399273b
Arkan Gilang <arkan.gilang@traveloka.com>
2024-03-22 23:18:24 +0700

    Refactor MUIRichTextEditor custom controls

--- src/tabs/story.tsx
+++ src/tabs/story.tsx
@@ -1,11 +1,9 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { createTheme, styled, ThemeProvider } from '@mui/material/styles'
-import MUIRichTextEditor, { TCustomControl, TMUIRichTextEditorRef } from 'mui-rte'
-import {Lock} from '@mui/icons-material'
-import { ContentBlock, EditorState } from 'draft-js';
-import IconButton from '@mui/material/IconButton';
+import MUIRichTextEditor, { TMUIRichTextEditorRef } from 'mui-rte'
 import { gatherBlocks } from '../utils/ConversionUtils';
 import ProseNarrative from '../core/ProseNarrative';
+import { EditorState } from 'draft-js';
 
 
 const myTheme = createTheme({
@@ -22,54 +20,14 @@ const EditorContainer = styled('div')(({ theme }) => ({
     },
   }));
 
-  const actSelect: TCustomControl = {
-    name: 'select',
-    type: 'callback' as const,
-    onClick: (editorState: EditorState) => {
-      const selectionState = editorState.getSelection();
-      const startKey = selectionState.getStartKey();
-      const endKey = selectionState.getEndKey();
-      const startOffset = selectionState.getStartOffset();
-      const endOffset = selectionState.getEndOffset();
-      const contentState = editorState.getCurrentContent();
   
-      let selectedText = '';
-      let block: ContentBlock | undefined = contentState.getBlockForKey(startKey);
-  
-      // If the selection starts and ends in the same block
-      if (startKey === endKey) {
-        selectedText = block.getText().slice(startOffset, endOffset);
-      } else {
-        // Selection spans multiple blocks
-        let blockKey = startKey;
-  
-        while (block) {
-          const blockText = block.getText();
-          const key = block.getKey();
-  
-          if (key === startKey) {
-            selectedText += blockText.slice(startOffset);
-          } else if (key === endKey) {
-            selectedText += '\n' + blockText.slice(0, endOffset);
-            break; // No need to go further
-          } else {
-            selectedText += '\n' + blockText;
-          }
-  
-          block = contentState.getBlockAfter(key);
-        }
-      }
-  
-      console.log(selectedText);
-    },
-    icon: <Lock />, // Use your actual icon component
-  };
 
 const defaultControls = ["title", "bold", "italic", "underline", "strikethrough", "link", "media", 
   "quote", "code", "clear","save"];
 
 const Story: React.FC = () => {
     const [lock, setLock] = useState(false);
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
     const defText:ProseNarrative = new ProseNarrative("1", 0, 
       "*italic*\n**bold**\n~~strikethrough~~\n[link](https://www.google.com)");
@@ -91,7 +49,10 @@ const Story: React.FC = () => {
             // Convert the editor's content to raw state
             setTextval(data);
             const content = JSON.parse(data);
-            console.log(content);
+            let n = content.blocks.length;
+            for(let i=0; i<n; i++) {
+              // const narrative: ProseNarrative = new ProseNarrative()
+            }
         // Use a library or a custom function to convert `content` to Markdown
     };
     return (
@@ -100,10 +61,11 @@ const Story: React.FC = () => {
                 <MUIRichTextEditor label="Start typing..."
                     ref={ref}
                     readOnly={lock}
-                    customControls={[actSelect]}
+                    // customControls={[actSelect]}
                     controls={[...defaultControls, "select"]}
                     defaultValue={textval}
                     onSave={handleChange} 
+                    onChange={setEditorState}
                 />
             </EditorContainer>
         </ThemeProvider>
