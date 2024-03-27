9204c5a6a66cdd6960d71d59291ef0d77eb68686
Arkan Gilang <arkan.gilang@traveloka.com>
2024-03-27 13:18:34 +0700

    Refactor ProseNarrative class by removing unused code

--- src/core/ProseNarrative.ts
+++ src/core/ProseNarrative.ts
@@ -3,42 +3,6 @@ import { randomString } from "../utils/Random";
 import Narrative, {NarrativeDict, defaultSerializer} from "./Narrative";
 import narrativeFactory from "./NarrativeFactory";
 
-const proseToDraftjsSerializer = {
-    serialize: (narrative: ProseNarrative) => {
-        let {cleanText, styleRanges} = markdownToDraftjs(narrative.text);
-        let entityRanges: any[] = [];
-        let entityData:any = {};
-        for(let style of styleRanges) {
-            if (style.style === "LINK") {
-                entityData[entityRanges.length] = style.entity;
-                entityRanges.push({
-                    offset: style.offset,
-                    length: style.length,
-                    key: entityRanges.length
-                });
-            }
-        }
-        let blockData = {
-            key: narrative.id,
-            text: cleanText,
-            type: narrative.blockType,
-            depth: 0,
-            inlineStyleRanges: styleRanges,
-            entityRanges: entityRanges,
-            data: entityData
-        }
-        return JSON.stringify(blockData);
-    },
-    deserialize: (data: string, narrative: ProseNarrative) => {
-        console.log(data);
-        let blockData = JSON.parse(data);
-        console.log(blockData);
-        narrative.text = draftjsToMarkdown(blockData);
-        narrative.id = blockData.key;
-        narrative.blockType = blockData.type;
-    }
-}
-
 export default class ProseNarrative extends Narrative  {
     narrativeType: string = "prose";
     text: string = "";
@@ -50,11 +14,7 @@ export default class ProseNarrative extends Narrative  {
         this.id = id;
         this.timeline = timeline;
         this.text = text;
-
-        // this.addSerializer("draftjs", proseToDraftjsSerializer);
         this.addSerializer("default", defaultSerializer);
-        // if(blockType) text = JSON.stringify({text, blockType});
-        // this.deserialize("default", text);
     }
 }
 
