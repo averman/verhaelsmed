89257b0eb86354795b474025b63553e4e46d26a2
averman <averman@gmail.com>
2024-05-26 22:23:04 +0700

    fix build (3)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jsx-a11y/recommended',
-      'prettier',
-      'prettier/@typescript-eslint',
+    //   'plugin:react/recommended',
+    //   'plugin:@typescript-eslint/recommended',
+    //   'plugin:jsx-a11y/recommended',
     ],
     plugins: [
-      'react',
-      '@typescript-eslint',
-      'jsx-a11y',
-      'prettier',
+    //   'react',
+    //   '@typescript-eslint',
+    //   'jsx-a11y',
     ],
     rules: {
-      'prettier/prettier': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'eqeqeq': 'warn',
-      'no-eval': 'warn',
-      'no-throw-literal': 'warn',
-      'jsx-a11y/anchor-is-valid': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
+      "prefer-const": "off"
+    //   '@typescript-eslint/no-unused-vars': 'warn',
+    //   'eqeqeq': 'warn',
+    //   'no-eval': 'warn',
+    //   'no-throw-literal': 'warn',
+    //   'jsx-a11y/anchor-is-valid': 'warn',
+    //   'react-hooks/exhaustive-deps': 'warn',
+    //   "react-hooks/rules-of-hooks": "error",
     },
     settings: {
       react: {
